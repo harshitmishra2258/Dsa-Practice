11db3722cf3137060472234ea5563b5d70dcca2a
Harshit  Mishra <harshitmishra2258@gmail.com>
2026-02-07 22:00:39 +0530

    Add files via upload

---
+++ Hollowtriangle.java
@@ -0,0 +1,16 @@
+public class Hollowtriangle{
+    public static void HollowTriangle(int n) {
+        for (int i=1;i<=n;i++){
+            for (int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for (int j=1;j<=2i-1;j++){
+
+            }
+        }
+        
+    }
+    public static void main(String args[]) {
+        
+    }
+}
