a6fb93575f8cc17e7071b23a9660561bc9acefb4
Harshit  Mishra <harshitmishra2258@gmail.com>
2026-02-15 08:58:26 +0530

    Add files via upload

---
+++ StringQtwo.java
@@ -0,0 +1,8 @@
+public class StringQtwo {
+    public static void main(String[] args) {
+        String s1="hoho";
+        String s2="hohk".replace("1", "");
+        System.out.println(s2);
+    }
+    
+}
