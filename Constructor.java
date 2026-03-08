f382fe52ce97db4cdc5a020c1f5b69368e1856e8
Harshit  Mishra <harshitmishra2258@gmail.com>
2026-03-08 19:57:29 +0530

    Add files via upload

---
+++ Constructor.java
@@ -0,0 +1,14 @@
+public class Constructor {
+    public static void main(String[] args) {
+        Student s1=new Student("Harshit");
+        System.out.println(s1.name);
+    }    
+}
+class Student {
+    String name ;
+    int roll;
+
+    Student(String name){
+        this.name=name;
+    }
+}
